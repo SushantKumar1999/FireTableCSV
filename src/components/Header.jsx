71b30e3a409ca146be7ee0bdaa8d040d02fad1ae
Sushant Kumar <sushantkumar19990819@gmail.com>
2024-01-31 20:55:24 +0530

    final touch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import userimg from "../assets/image 1.png";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center mt-[49px] w-[1348px] h-[32px]">
+    <div className="flex justify-between items-center mt-[49px] h-[32px]">
       <h1 className="text-black font-bold text-[21px] sm:text-[24px] ">
         Upload CSV
       </h1>
-      <div className="flex justify-between items-center gap-3">
+      <div className="flex justify-between items-center gap-3" style={{float:"right"}}>
         <img src={bellimg} alt="bell" className="w-[18px] h-[20px]" />
         <img src={userimg} alt="user" className="rounded-full"/>
       </div>
